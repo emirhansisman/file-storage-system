c63de332f9c761cf4a61b98215dba520310eb535
emirhansisman <emirhansisman@gmail.com>
2022-04-10 00:01:00 +0300

    Added Swagger documentation to FileController methods

--- spring-boot-file-storage-rest-api/src/main/java/com/esisman/fileupload/controller/FileController.java
+++ spring-boot-file-storage-rest-api/src/main/java/com/esisman/fileupload/controller/FileController.java
@@ -3,6 +3,11 @@ package com.esisman.fileupload.controller;
 import com.esisman.fileupload.entity.File;
 import com.esisman.fileupload.model.FileResponseModel;
 import com.esisman.fileupload.service.FileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,10 @@ import java.util.stream.Collectors;
 public class FileController {
     private final FileStorageService fileStorageService;
 
+    @Operation(summary = "Get list of files' information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retrieved list of files' information successfully",
+                    content = @Content) })
     @GetMapping
     public ResponseEntity<List<FileResponseModel>> getFiles() {
         List<File> files = fileStorageService.listAllFiles();
@@ -29,6 +38,10 @@ public class FileController {
         return new ResponseEntity(fileResponseModels, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get file content as byte array")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Got file content as byte array",
+                    content = @Content) })
     @GetMapping("/{fileName}/content")
     public ResponseEntity<byte[]> getFileContent(@PathVariable String fileName) {
         final byte[] fileContent = fileStorageService.getFileContent(fileName);
@@ -36,6 +49,10 @@ public class FileController {
         return new ResponseEntity(fileContent, HttpStatus.OK);
     }
 
+    @Operation(summary = "Upload file to server")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File uploaded successfully",
+                    content = @Content) })
     @PostMapping("/upload")
     public ResponseEntity uploadFile(@RequestParam(name = "file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
@@ -45,6 +62,10 @@ public class FileController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete file from server")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "File deleted successfully",
+                    content = @Content) })
     @DeleteMapping("/{fileName}")
     public ResponseEntity deleteFile(@PathVariable String fileName) {
         fileStorageService.deleteFile(fileName);
