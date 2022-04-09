ad23686911431c269c9cbac1b3ebe981db8abc82
emirhansisman <emirhansisman@gmail.com>
2022-04-09 22:14:41 +0300

    OpenAPI dependency added to pom.xml and basic Swagger configuration setup

--- spring-boot-file-storage-rest-api/src/main/java/com/esisman/fileupload/FileUploadApplication.java
+++ spring-boot-file-storage-rest-api/src/main/java/com/esisman/fileupload/FileUploadApplication.java
@@ -1,9 +1,12 @@
 package com.esisman.fileupload;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "File Upload API", version = "2.0", description = "Spring Boot REST API to store, retrieve, update and delete specific file"))
 public class FileUploadApplication {
 
 	public static void main(String[] args) {
